c68b221985e1208d46ff1c96d85b8bb090d4ed68
D. Jamaatul Anbiya <djamaatul.anbiya@gmail.com>
2021-12-02 09:53:58 +0700

    make latihan 2

--- latihan1/src/component/Header.js
+++ latihan1/src/component/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+const styles = {
+    h1: {
+        color: 'red',
+    },
+}
+
+const {...s} = styles;
+
 const Header = () =>{
     return (
         <div className="header-container">
-            <h1>Welcome!</h1>
+            <h1 style={s.h1}>Welcome!</h1>
         </div>
     )
 }
+
 
 export default Header
